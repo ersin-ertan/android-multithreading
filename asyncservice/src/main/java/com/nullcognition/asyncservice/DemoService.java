bef493e40f04dfc7b75cd3edbc8c7ab3c16d3165
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-26 01:16:36 -0400

    added: tansitive require in initial commit

---
+++ asyncservice/src/main/java/com/nullcognition/asyncservice/DemoService.java
@@ -0,0 +1,8 @@
+package com.nullcognition.asyncservice;
+// ersin 26/08/15 Copyright (c) 2015+ All rights reserved.
+
+import com.joanzapata.android.asyncservice.api.annotation.AsyncService;
+
+@AsyncService
+public class DemoService{
+}
