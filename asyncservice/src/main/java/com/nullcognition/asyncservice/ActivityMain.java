c41a6a3ed146ad66db56496ce6e4be11c0a5b097
ersin-ertan <ersin_the_ertan@hotmail.com>
2015-08-26 03:26:45 -0400

    added: changed service call method name, uses context in demo service todo: understand the caching and priority of the calls because as of now, the logger sees all of the messages bu the OnMessage specific to the main thread only sees certain call - which is state in the docs to be only that of the call originating from the calling class... which may be the answer I was looking for.

--- asyncservice/src/main/java/com/nullcognition/asyncservice/ActivityMain.java
+++ asyncservice/src/main/java/com/nullcognition/asyncservice/ActivityMain.java
@@ -2,12 +2,17 @@ package com.nullcognition.asyncservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.joanzapata.android.asyncservice.api.annotation.InjectService;
 import com.joanzapata.android.asyncservice.api.annotation.OnMessage;
 import com.joanzapata.android.asyncservice.api.internal.AsyncService;
+
+import java.lang.ref.WeakReference;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,6 +24,7 @@ public class ActivityMain extends Activity{
 	@Bind(R.id.TextView_getString) TextView    getString;
 
 	@OnClick(R.id.Button_getString) void getString(final View view){
+//		A caller only receive the messages originated from its own requests by default
 		String s = demoService.getString("passed from activity");
 		if(null == s){getString.setText("string is null");}
 	}
@@ -30,9 +36,48 @@ public class ActivityMain extends Activity{
 		ButterKnife.bind(this);
 
 		AsyncService.inject(this);
+
+		handler.postDelayed(new Runnable(){
+			@Override public void run(){
+				demoService.getInt();
+
+			}
+		}, 2000);
 	}
 
-	@OnMessage public void onGetString(String result){
-		getString.setText(result);
+	private final MyHandler handler = new MyHandler(this);
+
+
+	private static class MyHandler extends Handler{
+
+		private final WeakReference<ActivityMain> activity;
+
+		public MyHandler(ActivityMain activity){
+			this.activity = new WeakReference<ActivityMain>(activity);
+		}
+
+		@Override
+		public void handleMessage(Message msg){
+			ActivityMain activity = this.activity.get();
+			if(activity != null){
+			}
+		}
+	}
+
+	@OnMessage public void onFromService(Object result){ // will catch all objects from the service,
+		// but if typed ex.String will only catch String results
+		getString.setText(result.toString());
+	}
+
+//	@OnMessage public void getInt(){demoService.getInt();} // cannot have multiple @OnMessage annotations
+
+	@OnMessage(from = OnMessage.Sender.ALL) public void logger(Object object){
+		Toast.makeText(ActivityMain.this, "Logging:" + object.toString(), Toast.LENGTH_SHORT).show();
 	}
 }
+
+// odd behaviour to investigate, when pushing the button then leaving the app, the toast will trigger
+// as expected and also if you enter and exit the app quickly, but if you enter press, exit, enter,
+// the handlers request will occur first and block the getString result from populating the text field
+// but it will still be seen by the logger. If you do multiple presses they start to get cached and
+// will replay an increment amount of times.
--- asyncservice/src/main/java/com/nullcognition/asyncservice/DemoService.java
+++ asyncservice/src/main/java/com/nullcognition/asyncservice/DemoService.java
@@ -1,6 +1,9 @@
 package com.nullcognition.asyncservice;
 // ersin 26/08/15 Copyright (c) 2015+ All rights reserved.
 
+import android.content.Context;
+
+import com.joanzapata.android.asyncservice.api.annotation.ApplicationContext;
 import com.joanzapata.android.asyncservice.api.annotation.AsyncService;
 import com.joanzapata.android.asyncservice.api.annotation.Init;
 
@@ -8,8 +11,9 @@ import com.joanzapata.android.asyncservice.api.annotation.Init;
 public class DemoService{
 
 	public DemoService(){}
-//	@ApplicationContext
-//	static Context context;
+
+	@ApplicationContext
+	static Context context;
 
 	// not a singleton thus all field must be static
 	// RestClient client
@@ -23,11 +27,16 @@ public class DemoService{
 	public String getString(String string){
 
 //		// runs asynchronously
-		try{ Thread.sleep(1000);}
+		try{ Thread.sleep(5000);}
 		catch(InterruptedException e){ e.printStackTrace();}
+
+		String resString = context.getResources().getString(R.string.resString);
 //
-		return string + stringGenerator.generate();
+		return string + "\n" + stringGenerator.generate() + "\n" + resString;
 		// returning null here will not propagate
 		// the message
 	}
+
+	 // type in method return must be of type object, no primitives
+	public Integer getInt(){return 5; }
 }
